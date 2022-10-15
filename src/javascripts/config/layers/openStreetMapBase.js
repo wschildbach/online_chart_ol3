eec49123878ac950449b4f1aedf21d18d1b96890
Wolfgang Schildbach <wschildbach@fermi.franken.de>
2022-10-15 20:32:19 +0200

    use t2.openseamap.org as tile server

--- src/javascripts/config/layers/openStreetMapBase.js
+++ src/javascripts/config/layers/openStreetMapBase.js
@@ -20,7 +20,8 @@ export const messages = defineMessages({
 
 module.exports = function (context, options) {
   let source = new ol.source.OSM({
-    url: '//{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+//    url: '//tile.openstreetmap.org/{z}/{x}/{y}.png',
+    url: '//t2.openseamap.org/tile/{z}/{x}/{y}.png',
 //    url: '//mapproxy.195.37.132.70.xip.io/tiles/1.0.0/osm/osm_grid/{z}/{x}/{y}.png',
     crossOrigin: 'Anonymous'
   })
