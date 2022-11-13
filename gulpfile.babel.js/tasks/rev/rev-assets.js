cadfbd9929548b4b31a2dca7d91b86ce2b6624f1
Wolfgang Schildbach <wschi@dolby.com>
2022-11-13 10:49:11 +0100

    reverted changes in rev-assets.js

--- gulpfile.babel.js/tasks/rev/rev-assets.js
+++ gulpfile.babel.js/tasks/rev/rev-assets.js
@@ -1,18 +1,18 @@
 var config = require('../../config')
-var {task,src,dest} = require('gulp')
+var gulp = require('gulp')
 var path = require('path')
-var {rev} = require('gulp-rev')
+var rev = require('gulp-rev')
 var revNapkin = require('gulp-rev-napkin')
 
 // 1) Add md5 hashes to assets referenced by CSS and JS files
-task('rev-assets', function () {
+gulp.task('rev-assets', function () {
   // Ignore files that may reference assets. We'll rev them next.
   var ignoreThese = '!' + path.join(config.root.dest, '/**/*+(css|js|json|html)')
 
-  return src([path.join(config.root.dest, '/**/*'), ignoreThese])
+  return gulp.src([path.join(config.root.dest, '/**/*'), ignoreThese])
     .pipe(rev())
-    .pipe(dest(config.root.dest))
+    .pipe(gulp.dest(config.root.dest))
     .pipe(revNapkin({verbose: false}))
     .pipe(rev.manifest(path.join(config.root.dest, 'rev-manifest.json'), {merge: true}))
-    .pipe(dest(''))
+    .pipe(gulp.dest(''))
 })
